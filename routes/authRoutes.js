d68ea46050b0ec2f74375b175131fd1824889702
sheldon fam <sheldonfam95@gmail.com>
2024-07-19 00:11:08 +0800

    add car listing api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,12 +6,16 @@ const {
   updateProfile,
 } = require("../controllers/profileController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const { getCarList, addCar } = require("../controllers/carController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/getprofile", getProfile);
 router.post("/updateprofile", authMiddleware, updateProfile);
-// router.get("/cardlist",)
+
+//Car
+router.get("/cardlist", getCarList);
+router.post("/addcard", addCar);
 
 module.exports = router;
